8d88bf563edb1541a99e9cc2f7157ac335d903d0
hakanerdemtemiz <hakanerdemtemiz@gmail.com>
2018-09-02 14:49:26 +0300

    Add project files.

---
+++ hetMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hetMVC.Startup))]
+namespace hetMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
